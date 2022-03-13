ee99ce78175689e973de38a695cb6272287d8175
eleanor-miller <eleanormiller08@icloud.com>
2022-03-13 14:57:45 -0400

    create basic button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import { SlButton } from "@shoelace-style/shoelace/dist/react";
+
+const Button = () => (
+  <>
+    <SlButton className="orange" size="large" href="https://google.com/">
+      Button
+    </SlButton>
+  </>
+);
+
+export default Button;
