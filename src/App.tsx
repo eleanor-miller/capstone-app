9f0c5db749756428fb7eaf45310f16bcd86c5377
eleanor-miller <eleanormiller08@icloud.com>
2022-03-13 14:56:34 -0400

    install shoelace-style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import logo from "/src/images/logo.svg";
 import "./App.css";
 
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+
+setBasePath(
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.71/dist/"
+);
+
 function App() {
   const [count, setCount] = useState(0);
 
